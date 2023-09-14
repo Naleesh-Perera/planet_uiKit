5f3cb61292f6e922aedf9ab2511aa4008a8fbc9d
Naleesh Jeffrey <naleeshp@treinetic.com>
2023-09-14 10:11:53 +0530

    Initial Commit

---
+++ Planet_uiKitUITests/Planet_uiKitUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Planet_uiKitUITestsLaunchTests.swift
+//  Planet_uiKitUITests
+//
+//  Created by Naleesh Jeffrey on 2023-09-14.
+//
+
+import XCTest
+
+final class Planet_uiKitUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
